b0696001c18adc80ad7f69cb46415673adbc1031
Kaev <demonhunter@hotmail.de>
2025-07-16 22:37:17 +0200

    Fixed baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://kaev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/classic-emulation-wiki/',
+  baseUrl: '/classic-emulation-tutorials/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
